249a328746aee1345a3afd011b01f6cbc5cdb801
Vasiliy Gasevskiy <gasevskyv@gmail.com>
2020-02-05 15:31:48 +0700

    Initial commit

---
+++ src/main/java/com/gmail/gasevskyV/tracker/controller/RegController.java
@@ -0,0 +1,12 @@
+package com.gmail.gasevskyV.tracker.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class RegController {
+    @RequestMapping(/re)
+    public String regPage(){
+        return null;
+    }
+}
