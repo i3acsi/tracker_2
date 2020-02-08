b0fa5b4016121f2054331c35c20c641ba958cff1
Vasiliy Gasevskiy <gasevskyv@gmail.com>
2020-02-08 13:46:05 +0700

    edit user

--- src/main/java/com/gmail/gasevskyV/tracker/controller/UserController.java
+++ src/main/java/com/gmail/gasevskyV/tracker/controller/UserController.java
@@ -5,18 +5,19 @@ import com.gmail.gasevskyV.tracker.entity.User;
 import com.gmail.gasevskyV.tracker.repository.UserRepo;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 @Controller
 @RequestMapping("/user")
 @AllArgsConstructor
 @Slf4j
+//@PreAuthorize("hasAuthority('ROLE_ADMIN')")
 public class UserController {
     private final UserRepo userRepo;
 
@@ -47,21 +48,55 @@ public class UserController {
 
     @PostMapping
     public String changeUser(@AuthenticationPrincipal User userAuth,
-//
+                             @RequestParam Map<String, Object> map,
+                             Model model) {
+        if (userAuth.getRoles().contains(Role.ROLE_ADMIN)) {
+            Long id = Long.valueOf(map.get("user_id").toString());
 
-                             @RequestParam String username,
-                             @RequestParam String surname,
-                             @RequestParam String old_password,
-                             @RequestParam String new_password,
-                             @RequestParam Map<String, String> map,
-//                             @RequestParam String isActive,
-                             Model model){
+            Optional<User> optionalUser = userRepo.findById(id);
+            User user = null;
+            l:
+            if (optionalUser.isPresent()) {
+                user = optionalUser.get();
 
-        log.info("username" + username);
-        log.info("old" +old_password);
-        log.info("new" + new_password);
-        map.forEach((key, value)-> log.info("key: "+ key +" value: " + value));
-//        log.info("isActive " + isActive);
-        return "userEdit";
+                String newUsername = map.getOrDefault("username", user.getUsername()).toString();
+                String newSurname = map.getOrDefault("surname", user.getSurname()).toString();
+                String newPassword = map.getOrDefault("new_password", "").toString();
+                String oldPassword = map.getOrDefault("old_password", "").toString();
+                log.info("username:" + newUsername);
+                log.info("surname:" + newSurname);
+                log.info("newpas:" + newPassword);
+                log.info("oldpas:" + oldPassword);
+                if (!newPassword.equals("")) {
+                    if (oldPassword.equals(user.getPassword())) {
+                        user.setPassword(newPassword);
+                    } else {
+                        model.addAttribute("message", "wrong password");
+                        break l;
+                    }
+                }
+                user.setUsername(newUsername);
+                user.setSurname(newSurname);
+                user.setActive(map.containsKey("isActive"));
+                user.setRoles(getRoles(map));
+
+                userRepo.save(user);
+
+                model.addAttribute("message", "user updated");
+            }
+        }
+
+        return "redirect:/user";
+    }
+
+    private Set<Role> getRoles(Map<String, Object> map) {
+        List<Role> roles = Arrays.asList(Role.values());
+        Set<Role> result = new HashSet<>();
+        roles.forEach(role -> {
+            if (map.keySet().contains(role.name())) {
+                result.add(role);
+            }
+        });
+        return result;
     }
 }
--- src/main/java/com/gmail/gasevskyV/tracker/repository/UserRepo.java
+++ src/main/java/com/gmail/gasevskyV/tracker/repository/UserRepo.java
@@ -4,8 +4,12 @@ import com.gmail.gasevskyV.tracker.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepo extends JpaRepository<User, Long> {
     User findByUsername(String username);
+
+    Optional<User> findById(Long id);
 
 }
