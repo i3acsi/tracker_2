e936af792b95db70dc42ff73d46cc6e1beabf149
Vasiliy Gasevskiy <gasevskyv@gmail.com>
2020-02-18 08:57:57 +0700

    Email auth

--- src/main/java/com/gmail/gasevskyV/tracker/entity/User.java
+++ src/main/java/com/gmail/gasevskyV/tracker/entity/User.java
@@ -5,8 +5,7 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
-import java.util.Collection;
-import java.util.Set;
+import java.util.*;
 
 @Entity
 @Table(name = "usr")
@@ -58,5 +57,52 @@ public class User implements UserDetails {
     public boolean isEnabled() {
 
         return getActive();
+    }
+
+    private void dosmth(Runnable r){
+        r.run();
+    }
+
+}
+
+class S{
+    static int i = 1;
+    static {
+        System.out.println("static S, i =" +i +" b= " + "not init");
+        i=4;
+        b=9;
+    }
+    static final int b;
+    int a = 4;
+    {
+        System.out.println("S non static, a="+a);
+        a=6;
+    }
+    public S(){
+        System.out.println("constructor S, i= "+ i + " a= "+a+" b="+b);
+    }
+}
+
+class C extends S{
+    static int ci = 99;
+    static {
+        System.out.println("static C, ci =" +ci +" cb= " + "not init");
+        ci=44;
+        cb=99;
+    }
+    static final int cb;
+    int ca = 4;
+    {
+        System.out.println("C non static, ca="+ca);
+        ca=6;
+    }
+    public C(){
+        System.out.println("constructor C, ci= "+ ci + " ca= "+ca+" cb="+cb);
+    }
+}
+
+class M{
+    public static void main(String[] args) {
+        C c = new C();
     }
 }
